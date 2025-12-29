4ca48a6ded167ce44078f7effa44115f89812d22
Nilesh Raju Ambekar <nileshambekar9281@gmail.com>
2025-12-30 00:06:27 +0530

    fix : use client issue -> added metadata to all the available pages.

--- cybershield-client/src/app/blog/page.tsx
+++ cybershield-client/src/app/blog/page.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blogs - Abhaya IT Solutions",
+  description:
+    "Read our latest blogs on cybersecurity and IT services.",
+};
+
 export default function Blog(){
     return (
         <>
